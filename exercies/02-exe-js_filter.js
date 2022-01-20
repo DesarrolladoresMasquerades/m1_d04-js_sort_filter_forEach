1636c82b8086f279a281a2b61b4dfa28b527312d
TA WDFT BCN Ironhack <ta-web-bcn@ironhack.com>
2022-01-20 12:43:19 +0100

    Update 02-exe-js_filter.js

--- exercies/02-exe-js_filter.js
+++ exercies/02-exe-js_filter.js
@@ -3,12 +3,12 @@
 //Exercise 1: Given an array of numbers, filter out the numbers that are not even, and are less than 100.
 const numbers = [1, 60, 112, 123, 100, 99, 73, 35];
 
-const evenNumbers = (arr) => {
-   //your code here...
-}
 
-let myEvenNumbers = evenNumbers(numbers)
-console.log(myEvenNumbers) // [1,99,73,35]
+const newArray = numbers.filter(number => {
+    return number % 2 === 1 && number < 100;
+  });
+  // in one line: const newArray = numbers.filter(number => number % 2 === 1 && number < 100);
+  console.log(newArray); // [1,99,73,35]
 
 // Exercise 2: From the given array of people, get the people who are allowed to consume alcoholic beverages (i.e age > 21).
 const people = [
@@ -20,12 +20,12 @@ const people = [
   { name: "Bill", age: 19 }
 ];
 
-const adults = (arr) => {
-   //your code here...
-}
+const adults = people.filter(person => {
+  return person.age > 21;
+});
 
-let canConsume = adults(people)
-console.log(canConsume) 
+//let canConsume = adults(people)
+console.log(adults) 
 
 /* Answer should be 
 [
@@ -38,7 +38,7 @@ console.log(canConsume)
 // Bonus: Using the same array above Just return the first two names of people eligible to drink. (i.e age > 21).
 // might need to use a .map() here later at some point
 // might also need to use another array method here which we haven't seen. Google it!, Again its a "MIGHT"
-const people = [
+const people2 = [
   { name: "Candice", age: 25 },
   { name: "Tammy", age: 30 },
   { name: "Allen", age: 49 },
@@ -47,10 +47,12 @@ const people = [
   { name: "Bill", age: 19 }
 ];
 
-const adults = (arr) => {
-   //your code here...
-}
+const canConsumeTwo  = adults.slice(0, 2).map(person => {
+  return person.name;
+});
 
-let canConsumeTwo = adults(people)
-console.log(canConsumeTwo) 
+// in one line: const namesArrOfAllowedToDrink = allowedToDrink.map(person => person.name);
+
+console.log(canConsumeTwo); 
+
 //Answer should be ['Candice', 'Tammy']
